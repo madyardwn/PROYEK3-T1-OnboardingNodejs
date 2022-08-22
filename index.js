e60295078ec45e8db951320b3abb0740890b99c1
madyardwn <madyardwn@gmail.com>
2022-08-22 14:42:13 +0700

    second commit

--- index.js
+++ index.js
@@ -2,3 +2,9 @@ console.log("Hello Node.js");
 
 const fs = require("fs");
 fs.writeFileSync("notes.txt", "I live in Philadelphia");
+
+const checkUtils = require("./src/utils.js");
+checkUtils();
+
+const validator = require("validator");
+console.log(validator.isURL("https/mead.io")); // Print: true
