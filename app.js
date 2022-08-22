092b7140286834bec5e34c4ba1efb89ef08d4d82
madyardwn <madyardwn@gmail.com>
2022-08-22 16:01:26 +0700

    four commit

--- app.js
+++ app.js
@@ -15,3 +15,34 @@ yargs.command({
   },
 });
 console.log(yargs.argv);
+
+yargs.command({
+  command: "add",
+  describe: "Add a new note",
+  builder: {
+    title: {
+      describe: "Note title",
+      demandOption: true,
+      type: "string",
+    },
+    body: {
+      describe: "Note body",
+      demandOption: true,
+      type: "string",
+    },
+  },
+  handler: function (argv) {
+    console.log("Title: " + argv.title);
+    console.log("Body: " + argv.body);
+  },
+});
+
+const book = {
+  title: "Ego is the Enemy",
+  author: "Ryan Holiday",
+};
+// Covert JavaScript object into JSON string
+const bookJSON = JSON.stringify(book);
+// Covert JSON string into object
+const bookObject = JSON.parse(bookJSON);
+console.log(bookObject.title); // Print: Ego is the Enemy
